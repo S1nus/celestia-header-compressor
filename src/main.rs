06dd339a442888f70ac0d887a23d908da2714dcf
Connor O'Hara <connor@switchboard.xyz>
2023-05-23 23:51:57 -0400

    got it to almost work... might fork tm-protos

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,70 @@
 use std::fs;
 use std::str::FromStr;
-use tendermint::block::signed_header::SignedHeader;
-use tendermint::block::{Commit, Id};
-use tendermint::account::{Id as AccountId};
-use tendermint::validator::Set as ValidatorSet;
-use tendermint::{Time};
+use tendermint_proto::v0_37::{
+    types::{
+        BlockId as RawBlockId, 
+        Header as RawHeader,
+        Commit as RawCommit,
+        ValidatorSet as RawValidatorSet,
+    },
+    version::Consensus as RawConsensusVersion,
+};
 use serde::{Deserialize, Serialize};
-use serde_with::{serde_as,DisplayFromStr};
 
-#[derive(Deserialize)]
-struct HeaderResponse {
-    jsonrpc: String,
-    result: CelestiaHeader,
-    id: usize,
-}
-
-/// AppHash is usually a SHA256 hash, but in reality it can be any kind of data
-#[derive(Clone, PartialEq, Eq, Default, Debug, Hash, Deserialize)]
-pub struct AppHash(Vec<u8>);
-
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct DAH {
-    row_roots: Vec<Vec<u8>>,
-    column_roots: Vec<Vec<u8>>,
+    row_roots: Vec<String>,
+    column_roots: Vec<String>,
 }
-
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct CelestiaHeader {
-    header: Header,
-    commit: Commit,
-    validators: ValidatorSet,
+    header: RawHeader,
+    commit: RawCommit,
+    validator_set: RawValidatorSet,
     dah: DAH,
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Deserialize)]
-pub struct Version {
-    /// Block version
-    #[serde_as(as = "DisplayFromStr")]
-    pub block: u64,
-
-    /// App version
-    #[serde_as(as = "DisplayFromStr")]
-    pub app: u64,
-}
-
-pub const SHA256_HASH_SIZE: usize = 32;
-
-/// Hash digests
-#[derive(Copy, Clone, Hash, Eq, PartialEq, PartialOrd, Ord, Default, Debug, Deserialize)]
-pub enum Hash {
-    /// SHA-256 hashes
-    Sha256([u8; SHA256_HASH_SIZE]),
-    /// Empty hash
-    #[default]
-    None,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Hash, Deserialize)]
-pub struct Header {
-    /// Header version
-    pub version: Version,
-
-    /// Chain ID
-    pub chain_id: String,
-
-    /// Current block height
-    pub height: u64,
-
-    /// Current timestamp
-    pub time: Time,
-
-    /// Previous block info
-    pub last_block_id: Option<Id>,
-
-    /// Commit from validators from the last block
-    pub last_commit_hash: Option<Hash>,
-
-    /// Merkle root of transaction hashes
-    pub data_hash: Option<Hash>,
-
-    /// Validators for the current block
-    pub validators_hash: Hash,
-
-    /// Validators for the next block
-    pub next_validators_hash: Hash,
-
-    /// Consensus params for the current block
-    pub consensus_hash: Hash,
-
-    /// State after txs from the previous block
-    pub app_hash: AppHash,
-
-    /// Root hash of all results from the txs from the previous block
-    pub last_results_hash: Option<Hash>,
-
-    /// Hash of evidence included in the block
-    pub evidence_hash: Option<Hash>,
-
-    /// Original proposer of the block
-    pub proposer_address: AccountId,
+#[derive(Deserialize)]
+struct Response {
+    jsonrpc: String,
+    result: CelestiaHeader,
+    id: u32,
 }
 
 fn main() {
-    let data = fs::read("header.json")
+    let data = r#"
+{
+            "version": {
+                "block": "11",
+                "app": "1"
+            },
+            "chain_id": "blockspacerace-0",
+            "height": "517435",
+            "time": "2023-05-18T16:52:21.114866275Z",
+            "last_block_id": {
+                "hash": "0F8465E11DDE3F1A794117E1C1BC521F80B1EBA694B83C1E90777CA36A66C1A0",
+                "parts": {
+                    "total": 1,
+                    "hash": "8087F8624305DD88B00FCD6F19AAC7E319FA5AF03919CEF4E57BC58563499E25"
+                }
+            },
+            "last_commit_hash": "ABAACE23204A783B197A8628C646DE3ABA105FCDB4C0B65182331FC64307342B",
+            "data_hash": "A407A4ACF12B54AF0E96400CCD698E524F4E3DC3D4F6E1249813D9F78A0EF0FD",
+            "validators_hash": "AA6B66F4F8224415CD48BD64F1016FF84D15CFAE8E6EDF391292ED26207C9334",
+            "next_validators_hash": "AA6B66F4F8224415CD48BD64F1016FF84D15CFAE8E6EDF391292ED26207C9334",
+            "consensus_hash": "048091BC7DDC283F77BFBF91D73C44DA58C3DF8A9CBC867405D8B7F3DAADA22F",
+            "app_hash": "ACCB2033E95DDBC051CF2F03E09B423B3D17A5C1DF3F6E675BC89084788FE94A",
+            "last_results_hash": "1A20C58F41547BF8CA9B9FBE323A0B57CFF60D701F8D15E25887B5264072AB8B",
+            "evidence_hash": "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855",
+            "proposer_address": "2AA44B697AB328C549087868F916B7A60C26B4FF"
+        }
+    "#;
+
+    let head: RawHeader = serde_json::from_str(data).unwrap();
+    println!("{:?}", head.height);
+
+    let file = fs::read("header.json")
         .unwrap();
-    let resp: HeaderResponse = serde_json::from_slice(data.as_slice()).unwrap();
-    println!("rpc {:?}", resp.jsonrpc);
+    let resp: Response = serde_json::from_slice(file.as_slice()).unwrap();
+    println!("{:?}", resp.result.header.chain_id);
 }
